81a66149cea0179218d1e9ab9f20c29cf64f8ee1
Raveen K Sharma <raveen08@gmail.com>
2021-01-21 20:52:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const driver = { name: 'Sam' };
+
+//created new object
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue (obj, key, value) {
+  obj[key] = value;
+ 
+  return obj;
+}
+
+//the empty {} in nondestructive is an empty object, so no overwrite, makes new Object
+
+function deleteFromDriverByKey(obj, key){
+  const newDriv = Object.assign({}, obj[key]);
+  delete newDriv[key];
+  return newDriv;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
